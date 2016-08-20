7f308ba2af0a7e3de0025969a096639c1966819d
railsbug <railsbug@126.com>
2016-08-20 21:03:36 +0800

    动画已经添加

--- js/js.js
+++ js/js.js
@@ -2,8 +2,28 @@
  * Created by 段段 on 2016/8/20.
  */
 $(function () {
-    $('.selectpicker').selectpicker({
+    // select美化
+    var selectAnim = $('.selectpicker').selectpicker({
         style: 'btn-info',
         size: 20
     });
+
+    // 动画联动
+    function testAnim(x) {
+        $('.animationSandbox').removeClass().addClass(x + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+            $(this).removeClass().addClass('animationSandbox');
+        });
+    };
+
+    $('.triggerAnimation').click(function(e){
+        e.preventDefault();
+        var anim = selectAnim.val();
+        testAnim(anim);
+    });
+
+    $('.selectAnimations').change(function(){
+        var anim = $(this).val();
+        testAnim(anim);
+    });
+
 })
